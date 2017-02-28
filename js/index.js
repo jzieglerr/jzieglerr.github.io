011cf28082393a188d17f48647c59997370ee60a
Jessica Ziegler <jbirdjaz@latins-MacBook-Pro.local>
2017-02-28 14:22:45 -0600

    scaled images, gave links functionality, centered header text

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$("#chocolate").on("click", function(){
+	window.open(chocolate.html)
+});
+
+$("#fruit").on("click", function(){
+	window.open(fruit.html)
+});
+
+$("#salt").on("click", function(){
+	window.open(salt.html)
+});
+
+$("#coffee").on("click", function(){
+	window.open(coffee.html)
+});
