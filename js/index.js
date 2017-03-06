d571b457bb6b87aeeb0e36b9fd4858eb818bf7a6
Jessica Ziegler <jbirdjaz@latins-MacBook-Pro.local>
2017-03-05 20:20:56 -0600

    page outlines complete, need to fix formatting and add suggestion box

--- js/index.js
+++ js/index.js
@@ -10,6 +10,27 @@ $("#salt").on("click", function(){
 	window.open(salt.html)
 });
 
-$("#coffee").on("click", function(){
-	window.open(coffee.html)
+
+$("#chocolate-img").on("mouseover", function(){
+	$("#chocolate-img").css("opacity", ".5")
+});
+
+$("#chocolate-img").on("mouseout", function(){
+	$("#chocolate-img").css("opacity", "1")
+});
+
+$("#fruit-img").on("mouseover", function(){
+	$("#fruit-img").css("opacity", ".5")
+});
+
+$("#fruit-img").on("mouseout", function(){
+	$("#fruit-img").css("opacity", "1")
+});
+
+$("#salt-img").on("mouseover", function(){
+	$("#salt-img").css("opacity", ".5")
+});
+
+$("#salt-img").on("mouseout", function(){
+	$("#salt-img").css("opacity", "1")
 });
