6fd7aa4c19596ea4cecd8baa089f4650d7ceedaf
Jessica Ziegler <jbirdjaz@ls-cart1-21.latin.latinschool.org>
2017-03-06 07:46:57 -0600

    changed css to CSS

--- js/index.js
+++ js/index.js
@@ -34,4 +34,35 @@ $("#salt-img").on("mouseout", function(){
 	$("#salt-img").css("opacity", "1")
 });
 
+// $("img").on("mouseover", function(){
+// 	$("img").css("opacity", ".5")
+// });
+
+// $("img").on("mouseout", function(){
+// 	$("img").css("opacity", "1")
+// });
+
+$("#hazelnut-truffles-img").on("mouseover", function(){
+	$("#hazelnut-truffles-img").css("opacity", ".5")
+});
+
+$("#hazelnut-truffles-img").on("mouseout", function(){
+	$("#hazelnut-truffles-img").css("opacity", "1")
+});
+
+$("#oat-choc-chip-cookies-img").on("mouseover", function(){
+	$("#oat-choc-chip-cookies-img").css("opacity", ".5")
+});
+
+$("#oat-choc-chip-cookies-img").on("mouseout", function(){
+	$("#oat-choc-chip-cookies-img").css("opacity", "1")
+});
+
+$("#smores-brownies-img").on("mouseover", function(){
+	$("#smores-brownies-img").css("opacity", ".5")
+});
+
+$("#smores-brownies-img").on("mouseout", function(){
+	$("#smores-brownies-img").css("opacity", "1")
+});
 
